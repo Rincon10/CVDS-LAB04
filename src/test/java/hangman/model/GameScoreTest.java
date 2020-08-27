76306d5d4a83f09afa20cc967b9c9405b0da9a70
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2020-08-27 11:13:32 -0500

    Implementacion de pruebas

--- src/test/java/hangman/model/GameScoreTest.java
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -1,4 +1,8 @@
-package model;
+package hangman.model;
+
+import org.junit.Test;
+import hangman.model.*;
+import org.junit.Assert;
 
 /**
  * @author Iván Camilo Rincón Saavedra
@@ -34,8 +38,131 @@ package model;
  *               2)                         incorrectCount < 0                      Incorrect/ Throws NEGATIVE_NUMBERS  Exception
  *               3)                  incorrectCount = 0 ^  correctCount>=4                         Correct/ 500
  *               4)                  incorrectCount >= 0 ^  correctCount=0                         Correct/ 0
- *               5)                                                                      Correct/  score +(sum(5^correctCount) - incorrectCount*failure )
+ *               5)           incorrectCount > 0 ^  correctCoun>0              Correct/  score +(sum(5^correctCount) - incorrectCount*failure )
  *
 */
 public class GameScoreTest {
+    private GameScore original = new OriginalScore();
+    private GameScore bonus = new BonusScore();
+    private GameScore power = new PowerScore();
+
+    //*********************************************OriginalScore***********************************************
+
+    @Test
+    public void shouldTrowAnExceptionFromOriginal(){
+        try{
+            original.calculateScore(-1,-1);
+        }
+        catch( Exception e ){
+            Assert.assertTrue( true );
+        }
+    }
+
+
+    // Limit test with  incorrectCount = 0
+    @Test
+    public void shouldBe100OnOriginal() throws GameScoreException {
+        int score = original.calculateScore(100,0);
+        Assert.assertEquals(score,100);
+
+    }
+
+    // Limit test with  correctCount = 10
+    @Test
+    public void shouldBe50OnOriginal() throws GameScoreException {
+        int score = original.calculateScore(0,5);
+        Assert.assertEquals(score,50);
+
+    }
+     // Limit test with  incorrectCount = 10
+    @Test
+    public void shouldBe0OnOriginal() throws GameScoreException {
+        int score = original.calculateScore(0,10);
+        Assert.assertEquals(score,0);
+
+    }
+
+    // Limit test with  incorrectCount = 10
+    @Test
+    public void shouldBeMinimumOriginal1() throws GameScoreException {
+        int score = original.calculateScore(26,11);
+        Assert.assertEquals(score,0);
+
+    }
+
+    @Test
+    public void shouldBeMinimumOriginal2() throws GameScoreException {
+        int score = original.calculateScore(34,40);
+        Assert.assertEquals(score,0);
+
+    }
+
+    //*********************************************BonusScore***********************************************
+    @Test
+    public void shouldTrowAnExceptionFromBonus(){
+        try{
+
+            bonus.calculateScore(-1,-1);
+        }
+        catch( Exception e ){
+            Assert.assertTrue( true );
+        }
+    }
+
+    @Test
+    public void shouldBeMinimumBonus1() throws GameScoreException {
+        int score = bonus.calculateScore(10,20);
+        Assert.assertEquals(score,0);
+
+    }
+    // Limit test with  incorrectCount = 0 ^  correctCount = 0
+    @Test
+    public void shouldBeMinimumBonus2() throws GameScoreException {
+        int score = bonus.calculateScore(0,0);
+        Assert.assertEquals(score,0);
+
+    }
+
+    @Test
+    public void shouldBe100OnBonus() throws GameScoreException {
+        int score = bonus.calculateScore(20,20);
+        Assert.assertEquals(score,100);
+
+    }
+
+    //*****************************************PowerScore***************************************************
+    @Test
+    public void shouldTrowAnExceptionFromPower(){
+        try{
+            power.calculateScore(-1,-1);
+        }
+        catch( Exception e ){
+            Assert.assertTrue( true );
+        }
+    }
+
+    // Limit test with  incorrectCount = 0 ^  correctCount = 4
+    @Test
+    public void shouldBeMaxScorePower() throws GameScoreException {
+        int score = power.calculateScore(4,0);
+        Assert.assertTrue(score == 500);
+
+    }
+
+    @Test
+    public void  shouldBeMinimumScorePower() throws GameScoreException {
+        int score = power.calculateScore(0,15);
+        Assert.assertEquals(score,0);
+
+    }
+
+
+    @Test
+    public void  shouldBeNormalScorePower() throws GameScoreException {
+        int score = power.calculateScore(4,63);
+        Assert.assertEquals(score,276);
+
+    }
+
+
 }
